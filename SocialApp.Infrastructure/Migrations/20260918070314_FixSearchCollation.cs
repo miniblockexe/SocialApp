d253b0dcf5cdbf377e0e68b6f24737ca73d25602
Trần Tâm <trantamttkmini@gmail.com>
2026-09-18 14:08:44 +0700

    sửa tìm kiếm lỗi db

---
+++ SocialApp.Infrastructure/Migrations/20260918070314_FixSearchCollation.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSearchCollation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                ALTER TABLE ""Users"" 
+                    ALTER COLUMN ""Username"" TYPE text COLLATE pg_catalog.""default"",
+                    ALTER COLUMN ""FullName"" TYPE text COLLATE pg_catalog.""default"";
+
+                ALTER TABLE ""Posts"" 
+                    ALTER COLUMN ""Content"" TYPE text COLLATE pg_catalog.""default"";
+    ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                ALTER TABLE ""Users"" 
+                    ALTER COLUMN ""Username"" TYPE text,
+                    ALTER COLUMN ""FullName"" TYPE text;
+
+                ALTER TABLE ""Posts"" 
+                    ALTER COLUMN ""Content"" TYPE text;
+            ");
+        }
+    }
+}
